616735346869a864277991e5435836f064c16a39
Dom Rollo <drollo1@umbc.edu>
2017-11-01 20:08:45 -0400

    everything excepts the mutex locks

--- worst_hw_ever/hw4.c
+++ worst_hw_ever/hw4.c
@@ -1,310 +1,295 @@
 //***********************************************************
-//File Name: hw3.c
+//File Name: hw4.c
 //Author Name: Dominic Rollo
-//Assignment: Homework 3
+//Assignment: Homework 4
 //
-//  A simulation of groups of children collecting candy. The
-//	 simulation uses threads for each group and the restocking
-//   the houses. The args passed in are file name followed buy
-//   number of seconds to run.
+//  
 //
 //**************Outside Help*********************************
-//  https://www.tutorialspoint.com/c_standard_library/c_function_rand.htm
+//  
 //
 //***********************************************************
 //**************Questions************************************
-//  1. A group might get to a house alittle bit after the main 
-//   thread's second count.  If it does, the amount of candy will
-//   change while the old number was being displayed.
-//  2. The program could calculate when a group will get to a 
-//   at the second mark and wait for it to aquire candy before
-//   main thread prints.
+//  1.
 //***********************************************************
-#define _BSD_SOURCE
+//#define _BSD_SOURCE
 
 #include <stdio.h>
+#include <errno.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <signal.h>
+//#include <string.h>
+//#include <unistd.h>
+//#include <sys/types.h>
+//#include <sys/wait.h>
 #include <pthread.h>
 
+extern void hw4_test(void);
 
-typedef struct House{
-	int location[2];
-	int candy;
-}House;
+#define PAGES 12
+#define PAGE_SIZE 32
 
-typedef struct Group{
-	int start_house, cur_house, size, candy, going_to;
-}Group;
+char static main_memory[PAGES][PAGE_SIZE];
+int static mem_table[PAGES];
 
-typedef struct Instruction{
-	int house;
-	int candy;
-} Instruction;
-
-House neighborhood[10];
-int G, T, end, start_sim, end_sim, total_candy;
-Group *lil_monsters;
-Instruction *refill_lst;
-int lst_size, monster_size, instr_size;
-pthread_mutex_t prnt_lock, thread_lock, house_lock[10];
-
-
-#define START_SIZE 10
-
-// Puts line from stream
 //**************************************************************
-static int get_number(FILE *infile){
-	char input[5];
-	for (int i=0; i<5; i++)
-		input[i]='\0';
-	char c_val =fgetc(infile);
-	int i=0;
-	do{
-		input[i]=c_val;
-		c_val=fgetc(infile);
-		i++;
-	}while (c_val!='\n'&& c_val!=' '&& c_val!=EOF);	
-	if(c_val==EOF)
-		end = 1;
-	int result = atoi(input); 
-	return result;
-}
-
-// Parses out and saves houses
+//  Initilizes the memory to zeros and lables memory as free
 //**************************************************************
-static void house_parser(FILE *infile){
-		for (int i=0; i<10; i++){
-			neighborhood[i].location[0]=get_number(infile);
-			neighborhood[i].location[1]=get_number(infile);
-			neighborhood[i].candy=get_number(infile);
+static void initilize(void){
+	for(int i=0;i<PAGES;i++)
+		for(int j=0;j<PAGE_SIZE;j++)
+			main_memory[i][j]=0;
+
+	for(int i=0;i<PAGES;i++){
+		mem_table[i]=-1;
+	}
+}
+
+/**
+ * Write to standard input information about the current memory
+ * allocations.
+ *
+ * Display to standard output the following:
+ * - Memory contents, one frame per line, 12 lines total. Display the
+ *   actual bytes stored in memory. If the byte is unprintable (ASCII
+ *   value less than 32 or greater than 126), then display a dot
+ *   instead.
+ * - Current memory allocation table, one line of 12 columns. For each
+ *   column, display a 'f' if the frame is free, 'R' if reserved.
+ */
+void my_malloc_stats(void){
+	printf("Memory contents:\n");
+	for(int i=0;i<PAGES;i++){
+		printf("  ");
+		for(int j=0;j<PAGE_SIZE;j++){
+			if(main_memory[i][j]>=32&&main_memory[i][j]<=126)
+				printf("%c", main_memory[i][j]);
+			else
+				printf(".");
 		}
+		printf("\n");
+	}
+	printf("Memory allocations:\n  ");
+	for (int i=0;i<PAGES; i++){
+		if(mem_table[i]==-1)
+			printf("f");
+		else
+			printf("R");
+	}
+	printf("\n");
+
+	//*******************************TEST Mark<<<<<<<<<
+	printf("  ");
+	for(int i=0;i<PAGES; i++)
+		if(mem_table[i]==-1)
+			printf("*");
+		else
+			printf("%d", mem_table[i]);
+	printf("\n");
+	//*************************************************
 }
 
-// Parse out the group variable and store in an array
-static void group_parser(FILE *infile){
-	monster_size=START_SIZE;
-	for (int i=0; i<G;i++){
-		if (i>=monster_size){
-			printf("resize\n");
-			monster_size=START_SIZE+monster_size;
-			lil_monsters=realloc(lil_monsters, sizeof(Group)*monster_size);
+/**
+ * Allocate and return a contiguous memory block that is within the
+ * memory region. The allocated memory remains uninitialized.
+ *
+ * Search through all of available for the largest free memory region,
+ * then return a pointer to the beginning of the region. If multiple
+ * regions are the largest, return a pointer to the region closest to
+ * address zero.
+ *
+ * The size of the returned block will be at least @a size bytes,
+ * rounded up to the next 32-byte increment.
+ *
+ * @param size Number of bytes to allocate. If @c 0, your code may do
+ * whatever it wants; my_malloc() of @c 0 is "implementation defined",
+ * meaning it is up to you if you want to return @c NULL, segfault,
+ * whatever.
+ *
+ * @return Pointer to allocated memory, or @c NULL if no space could
+ * be found. If out of memory, set errno to @c ENOMEM.
+ */
+void *my_malloc(size_t size){
+	if(size==0){
+		return NULL;
+	}
+	unsigned int pages;
+	if(size%32==0)
+		pages = size/32;
+	else
+		pages = (size/32)+1;
+	int large=-1;
+	int large_size=0;
+	int temp=-1;
+	for(int i=0;i<PAGES;i++){
+		if(temp==-1&&mem_table[i]==-1)
+			temp=i;
+		else if(temp!=-1&&mem_table[i]!=-1){
+			if((i-temp)>large_size){
+				large = temp;
+				large_size = i-temp;
+			}
+			temp = -1;
 		}
-		lil_monsters[i].start_house=get_number(infile);
-		lil_monsters[i].cur_house=lil_monsters[i].start_house;
-		lil_monsters[i].size=get_number(infile);
 	}
-}
-
-// Parses out and saves instrustions in an array
-//**************************************************************
-static void instruction_parser(FILE *infile){
-	lst_size=START_SIZE;
-	int i=0;
-	do{
-		if (i>=lst_size){
-			printf("list resize\n");
-			lst_size=START_SIZE+lst_size;
-			refill_lst=realloc(refill_lst, sizeof(Instruction)*lst_size);
-		}
-		refill_lst[i].house=get_number(infile);
-		refill_lst[i].candy=get_number(infile);
-		
-		i++;
-	}while(!end);
-	instr_size = i-1;
-}
-
-// Prints out houses the 10 houses in order with there locations
-// and amount of current candy.
-//**************************************************************
-static void print_house(void){
-	printf("  House statuses:\n");
-	for (int i=0;i<10;i++){
-		printf("    %d @ ", i);
-		printf("(%d, %d): ", neighborhood[i].location[0], neighborhood[i].location[1]);
-		printf("%d available\n", neighborhood[i].candy);
-	}
-	printf("Total candy: %d\n", total_candy);
-}
-
-// Prints out group size, where they are going and 
-static void print_group(void){
-	printf("  Group statuses:\n");
-	for (int i=0;i<G;i++){
-		printf("    %d:    ", i);
-		printf("size %d, ", lil_monsters[i].size);
-		printf("going to %d, ",lil_monsters[i].going_to );
-		printf("collected %d\n", lil_monsters[i].candy);
+	if(temp!=-1&&(12-temp)>large_size){
+		large = temp;
+		large_size = 12-temp;
 	}
 
-}
-
-// Calls the methods that parse and store house, group and
-// instruction variables.
-static void parser(FILE *infile){
-	G =get_number(infile);
-	house_parser(infile);
-	group_parser(infile);
-	instruction_parser(infile);
-}
-
-//use thread to restock houses
-//************************************************************
-static void *restock(void *args) {
-	int i=0;
-	pthread_mutex_unlock(&thread_lock);
-	while(!start_sim){
-			usleep(1);
-		}
-	usleep(250000);
-	while(!end_sim && i<lst_size){
-		pthread_mutex_lock(&house_lock[refill_lst[i].house]);
-		neighborhood[refill_lst[i].house].candy+=refill_lst[i].candy;
-		pthread_mutex_unlock(&house_lock[refill_lst[i].house]);
-		pthread_mutex_lock(&prnt_lock);
-		printf("Neighborhood: added %d to house %d\n", refill_lst[i].candy, refill_lst[i].house);
-		pthread_mutex_unlock(&prnt_lock);
-		i++;
-		usleep(250000);
+	if (pages>large_size){
+		errno = ENOMEM;
+		return NULL;
 	}
-	return NULL;
+
+	for(int i=large; i<large+pages; i++)
+		mem_table[i]=large;
+
+	return &main_memory[large];
 }
 
-// Picks a random house by doing some math to a random number
-int random_house(int size, int current){
-	time_t t;
-	srand((unsigned) time(&t));
-	int result = rand()/size;
-	result = result*(current+1);
-	result = result%10;
-	result = abs(result);
-	return result;
-}
 
-int move_time(int house1, int house2){
-	int x1, x2, y1, y2;
-	x1 = neighborhood[house1].location[0];
-	y1 = neighborhood[house1].location[1];
-	x2 = neighborhood[house2].location[0];
-	y2 = neighborhood[house2].location[1];
-
-	int result = abs(x1-x2)+abs(y1-y2);
-	result = result * 250000;
-	return result;
-}
-
-static void collect_candy(int group, int house){
-	pthread_mutex_lock(&house_lock[house]);
-	if(neighborhood[house].candy>=lil_monsters[group].size){
-		neighborhood[house].candy=neighborhood[house].candy - lil_monsters[group].size;
-		lil_monsters[group].candy += lil_monsters[group].size;
-		total_candy+=lil_monsters[group].size;
-		//printf("*****group %d collected %d candy\n", group, lil_monsters[group].size);
-	}
-	else{
-		lil_monsters[group].candy += neighborhood[house].candy;
-		total_candy+= neighborhood[house].candy;
-		//printf("*****group %d collected %d candy\n", group, neighborhood[house].candy);
-		neighborhood[house].candy= 0;
-
-	}
-	pthread_mutex_unlock(&house_lock[house]);
-}
-
-//Sets up the groups when thier threads start
 //*************************************************************
-static void *group_up(void *args){
-	int group_num = *(int *)(args);
-	int move_to, time;
-	while(!end_sim){
-		do{
-			move_to = random_house(lil_monsters[group_num].size, lil_monsters[group_num].cur_house);
-		}while(move_to == lil_monsters[group_num].cur_house || move_to == lil_monsters[group_num].start_house);
-		time = move_time(lil_monsters[group_num].cur_house, move_to);
-		lil_monsters[group_num].going_to=move_to;
-		pthread_mutex_lock(&prnt_lock);
-		printf("Group %d: ", group_num); 
-		printf("from house %d ", lil_monsters[group_num].cur_house);
-		printf("to %d", move_to);
-		printf(" (Travel time = %d ms)\n", time/1000);
-		pthread_mutex_unlock(&prnt_lock);
-		if(!start_sim)
-			pthread_mutex_unlock(&thread_lock);
-		while(!start_sim){
-			usleep(1);
-		}
-		usleep(time);
-		lil_monsters[group_num].cur_house = move_to;
-		collect_candy(group_num, move_to);
+//  returns a -1 if the pointer is not the start of a block 
+//*************************************************************
+static int is_start(void *ptr){
+	for(int i=0;i<PAGES; i++){
+		if(mem_table[i]==i&&ptr==&main_memory[i])
+			return i;
 	}
-	return NULL;
+	return -1;
 }
 
-static void run_sim(){
-	end_sim=0;
-	start_sim=0;
-	total_candy=0;
-	pthread_mutex_init(&prnt_lock, NULL);
-	pthread_mutex_init(&thread_lock, NULL);
-	for( int k=0; k<10;k++)
-		pthread_mutex_init(&house_lock[k], NULL);
-	pthread_t threads[G+1];
-	int number[G];
-	for(int j=0; j<G;j++){
-		number[j]=j;
-		pthread_mutex_lock(&thread_lock);
-		pthread_create(threads + j, NULL, group_up, number+j);
+//*************************************************************
+//   returns number of pages in block
+//*************************************************************
+static int block_size(int start){
+	int size=0;
+	for(int i=start;mem_table[i]==start;i++)
+		size++;
+	return size;
+}
+
+/**
+ * Retrieve the size of an allocation block.
+ *
+ * If @a ptr is a pointer returned by my_malloc() or my_realloc(),
+ * then return the size of the allocation block. Because my_malloc()
+ * and my_realloc() round up to the next 32-byte increment, the
+ * returned value may be larger than the originally requested amount.
+ *
+ * @return Usable size pointed to by @a ptr, or 0 if @a ptr is not a
+ * pointer returned by my_malloc() or my_realloc() (such as @c NULL).
+ */
+size_t my_malloc_usable_size(void *ptr){
+	int temp=is_start(ptr);
+	if(ptr==NULL||temp==-1)
+		return 0;
+	temp = block_size(temp);
+	return temp*32;
+}
+
+/**
+ * Deallocate a memory region that was returned by my_malloc() or
+ * my_realloc().
+ *
+ * If @a ptr is not a pointer returned by my_malloc() or my_realloc(),
+ * then raise a SIGSEGV signal to the calling thread. Likewise,
+ * calling my_free() on a previously freed region results in a
+ * SIGSEGV.
+ *
+ * @param ptr Pointer to memory region to free. If @c NULL, do
+ * nothing.
+ */
+void my_free(void *ptr){
+	if(ptr!=NULL){
+		int temp=is_start(ptr);
+		if (temp>-1){
+			int size = block_size(temp);
+			for(int i=temp;i<temp+size;i++)
+				mem_table[i]=-1;
+		}
+		else
+			raise(SIGSEGV);
 	}
-	int i=0;
-	pthread_mutex_lock(&thread_lock);
-	pthread_create(threads+G, NULL, restock, NULL);
-	pthread_mutex_lock(&thread_lock);
-	while(i<T){
-		pthread_mutex_lock(&prnt_lock);
-		printf("After %d seconds:\n", i);
-		print_group();
-		print_house();
-		pthread_mutex_unlock(&prnt_lock);
-		start_sim=1;
-		usleep(1000000);
-		i++;
+}
+
+/**
+ * Change the size of the memory block pointed to by @a ptr.
+ *
+ * - If @a ptr is @c NULL, then treat this as if a call to
+ *   my_malloc() for the requested size.
+ * - Else if @a size is @c 0, then treat this as if a call to
+ *   my_free().
+ * - Else if @a ptr is not a pointer returned by my_malloc() or
+ *   my_realloc(), then send a SIGSEGV signal to the calling process.
+ *
+ * Otherwise reallocate @a ptr as follows:
+ *
+ * - If @a size is smaller than the previously allocated size, then
+ *   reduce the size of the memory block. Mark the excess memory as
+ *   available. Memory sizes are rounded up to the next 32-byte
+ *   increment.
+ * - If @a size is the same size as the previously allocated size,
+ *   then do nothing.
+ * - If @a size is greater than the previously allocated size, then
+ *   allocate a new contiguous block of at least @a size bytes,
+ *   rounded up to the next 32-byte increment. Copy the contents from
+ *   the old to the new block, then free the old block.
+ *
+ * @param ptr Pointer to memory region to reallocate.
+ * @param size Number of bytes to reallocate.
+ *
+ * @return If allocating a new memory block or if resizing a block,
+ * then pointer to allocated memory; @a ptr will become invalid. If
+ * freeing a memory region or if allocation fails, return @c NULL. If
+ * out of memory, set errno to @c ENOMEM.
+ */
+void *my_realloc(void *ptr, size_t size){
+	if(ptr==NULL)
+		return my_malloc(size);
+	else if(ptr!=NULL&&size==0)
+		free(ptr);
+	else{
+		int temp =is_start(ptr);
+		int bl_size = block_size(temp);
+		if(temp==-1)
+			raise(SIGSEGV);
+		else{
+			int pages;
+			if((size%32)==0)
+				pages = size/32;
+			else
+				pages = size/32 +1;
+			if(bl_size<pages){
+				void *temp_ptr=my_malloc(size);
+				int new = is_start(temp_ptr);
+				for(int i=0; i<bl_size;i++)
+					for(int j=0;j<PAGE_SIZE;j++)
+						main_memory[new+i][j]=main_memory[temp+i][j];
+				my_free(ptr);
+				return temp_ptr;
+			}else if(bl_size>pages){
+				printf("Shrinking block size: %d pages: %d\n", bl_size, pages);
+				for(int i=0;i<bl_size-pages;i++)
+					mem_table[temp+pages+i]=-1;
+			}
+
+		}
 	}
-	printf("After %d seconds:\n", i);
-	print_group();
-	print_house();
-	end_sim=1;
-	for(int i=0; i<=G;i++)
-		pthread_join(threads[i], NULL);
 }
 
 int main(int argc, char *argv[]){
+	initilize();
+	/*
+	mem_table[0]=0;
+	mem_table[3]=3;
+	mem_table[4]=3;
+	mem_table[8]=8;
+	my_malloc_stats();
+	my_malloc(120);
+	*/
+	hw4_test();
 
-	if (argc==1){
-		fprintf(stderr, "There are not enough arguments to run program.");
-		fprintf(stderr, "\nFix to look like: ./hw3 trick1.data 4\n");
-		return 1;
-	}
-	FILE *data;
-	if ((data= fopen(argv[1], "r"))==0){
-			fprintf(stderr, "File could not open\n");
-			return 1;
-		}
-
-	T=atoi(argv[2]);
-	monster_size=START_SIZE;
-	lst_size=START_SIZE;
-	lil_monsters=malloc(sizeof(Group)*monster_size);
-	refill_lst=malloc(sizeof(Instruction)*lst_size);
-	parser(data);
-	run_sim();
-	
-	//clean up
-	fclose(data);
-	free(lil_monsters);
-	free(refill_lst);
 	return 0;
 }
